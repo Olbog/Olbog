bfec14d31303ce8e4694988546bdac96b90a536f
Olbog <olbog2009@bk.ru>
2021-09-15 00:20:54 +0300

    HW1

--- HM#1.py
+++ HM#1.py
@@ -1,4 +1,11 @@
 # 1. Найти сумму S введенных с клавиатуры вещественных чисел
 
-a, b, c, d, e = map (int, input().split)
+# a, b, c, d, e = map (int, input().split)
+# print (a + b + c + d + e)
+
+a = int (input ())
+b = int (input ())
+c = int (input ())
+d = int (input ())
+e = int (input ())
 print (a + b + c + d + e)
